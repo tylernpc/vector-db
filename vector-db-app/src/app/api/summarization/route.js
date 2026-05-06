d0147a5952bcca3b36eec46e360e574da9d23af1
tylernpc <tylerlegm@gmail.com>
2026-05-05 22:16:05 -0500

    Embedding added

--- vector-db-app/src/app/api/summarization/route.js
+++ vector-db-app/src/app/api/summarization/route.js
@@ -1,6 +1,8 @@
 import { ChatOpenAI } from "@langchain/openai";
+import { OpenAI } from "openai";
 
 const model = new ChatOpenAI({ model: "gpt-4o-mini" });
+const openai = new OpenAI();
 
 export async function POST(request) {
   const { message } = await request.json();
@@ -10,7 +12,15 @@ export async function POST(request) {
   }
 
   try {
+    const embedding = await openai.embeddings.create({
+      model: "text-embedding-3-small",
+      input: "Your text string goes here",
+      encoding_format: "float",
+    });
+
     const response = await model.invoke([{ role: "user", content: message }]);
+
+    console.log(embedding);
 
     return Response.json({ result: response.content });
   } catch (err) {
