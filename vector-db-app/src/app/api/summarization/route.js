8d6bc0219759a8a5791d7c6b13a3e8208bc4cb34
tylernpc <tylerlegm@gmail.com>
2026-05-05 22:37:47 -0500

    Vector Search Changes

--- vector-db-app/src/app/api/summarization/route.js
+++ vector-db-app/src/app/api/summarization/route.js
@@ -3,6 +3,8 @@ import { OpenAI } from "openai";
 
 const model = new ChatOpenAI({ model: "gpt-4o-mini" });
 const openai = new OpenAI();
+const supabaseKey = process.env.SUPABASE_SECRET_KEY
+const url = process.env.SUPABASE_URL
 
 export async function POST(request) {
   const { message } = await request.json();
@@ -12,15 +14,13 @@ export async function POST(request) {
   }
 
   try {
-    const embedding = await openai.embeddings.create({
+    const embeddings = await openai.embeddings.create({
       model: "text-embedding-3-small",
       input: "Your text string goes here",
       encoding_format: "float",
     });
 
     const response = await model.invoke([{ role: "user", content: message }]);
-
-    console.log(embedding);
 
     return Response.json({ result: response.content });
   } catch (err) {
@@ -30,4 +30,20 @@ export async function POST(request) {
       { status: 500 },
     );
   }
+}
+
+export const run = async () => {
+  const client = createClient(url, supabaseKey)
+  const vectorStore = await SupabaseVectorStore.fromTexts(
+      ['Hello world', 'Bye bye', "What's this?"],
+      [{ id: 2 }, { id: 1 }, { id: 3 }],
+      new OpenAIEmbeddings(),
+      {
+        client,
+        tableName: 'documents',
+        queryName: 'match_documents',
+      }
+  )
+  const resultOne = await vectorStore.similaritySearch('Hello world', 1)
+  console.log(resultOne)
 }
